81cdecba886d7312dd66d9bb81290e881c24c30e
Andreas Buschka <kontakt@andreas-buschka.de>
2021-05-14 22:20:38 +0200

    Enable configuring the timeout until the new machine is expected to respond to SSH connections using the environment variables MACHINE_SSH_ATTEMPTS and MACHINE_SSH_RETRY_INTERVAL.

--- libmachine/drivers/utils.go
+++ libmachine/drivers/utils.go
@@ -2,10 +2,18 @@ package drivers
 
 import (
 	"fmt"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/rancher/machine/libmachine/log"
 	"github.com/rancher/machine/libmachine/mcnutils"
 	"github.com/rancher/machine/libmachine/ssh"
+)
+
+const (
+	DefaultSSHAttempts      = 60
+	DefaultSSHRetryInverval = 3
 )
 
 func GetSSHClientFromDriver(d Driver) (ssh.Client, error) {
@@ -62,8 +70,37 @@ func sshAvailableFunc(d Driver) func() bool {
 }
 
 func WaitForSSH(d Driver) error {
-	// Try to dial SSH for 30 seconds before timing out.
-	if err := mcnutils.WaitFor(sshAvailableFunc(d)); err != nil {
+	// Try to dial SSH for sshRetryInterval seconds before timing out.
+	var maxAttempts int
+	var retryInterval int
+	var err error
+
+	if maxAttemptsStr, envVarPresent := os.LookupEnv("MACHINE_SSH_ATTEMPTS"); envVarPresent {
+		if maxAttempts, err = strconv.Atoi(maxAttemptsStr); err != nil {
+			log.Errorf("Value '%s' from environment variable MACHINE_SSH_ATTEMPTS could not be parsed, "+
+				"setting default value (%d)",
+				maxAttemptsStr, DefaultSSHAttempts)
+			maxAttempts = DefaultSSHAttempts
+		}
+	} else {
+		maxAttempts = DefaultSSHAttempts
+	}
+
+	if retryIntervalStr, envVarPresent := os.LookupEnv("MACHINE_SSH_RETRY_INTERVAL"); envVarPresent {
+		if retryInterval, err = strconv.Atoi(retryIntervalStr); err != nil {
+			log.Errorf("Value '%s' from environment variable MACHINE_SSH_RETRY_INTERVAL could not be parsed, "+
+				"setting default value (%d)",
+				retryIntervalStr, DefaultSSHRetryInverval)
+			retryInterval = DefaultSSHRetryInverval
+		}
+	} else {
+		retryInterval = DefaultSSHRetryInverval
+	}
+
+	interval := time.Duration(retryInterval * int(time.Second))
+	log.Infof("Waiting for SSH to become available (maximum attempts: %d, interval between attempts: %s)",
+		maxAttempts, interval.String())
+	if err := mcnutils.WaitForSpecific(sshAvailableFunc(d), maxAttempts, interval); err != nil {
 		return fmt.Errorf("Too many retries waiting for SSH to be available.  Last error: %s", err)
 	}
 	return nil
