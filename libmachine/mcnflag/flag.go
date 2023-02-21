2191830c187522348fd69482af63de3d819953a0
Jake Hyde <jakefhyde@gmail.com>
2023-02-21 15:56:12 -0500

    Add optional flags

--- libmachine/mcnflag/flag.go
+++ libmachine/mcnflag/flag.go
@@ -5,13 +5,15 @@ import "fmt"
 type Flag interface {
 	fmt.Stringer
 	Default() interface{}
+	IsOptional() bool
 }
 
 type StringFlag struct {
-	Name   string
-	Usage  string
-	EnvVar string
-	Value  string
+	Name     string
+	Usage    string
+	EnvVar   string
+	Value    string
+	Optional bool
 }
 
 // TODO: Could this be done more succinctly using embedding?
@@ -23,11 +25,16 @@ func (f StringFlag) Default() interface{} {
 	return f.Value
 }
 
+func (f StringFlag) IsOptional() bool {
+	return f.Optional
+}
+
 type StringSliceFlag struct {
-	Name   string
-	Usage  string
-	EnvVar string
-	Value  []string
+	Name     string
+	Usage    string
+	EnvVar   string
+	Value    []string
+	Optional bool
 }
 
 // TODO: Could this be done more succinctly using embedding?
@@ -39,11 +46,16 @@ func (f StringSliceFlag) Default() interface{} {
 	return f.Value
 }
 
+func (f StringSliceFlag) IsOptional() bool {
+	return f.Optional
+}
+
 type IntFlag struct {
-	Name   string
-	Usage  string
-	EnvVar string
-	Value  int
+	Name     string
+	Usage    string
+	EnvVar   string
+	Value    int
+	Optional bool
 }
 
 // TODO: Could this be done more succinctly using embedding?
@@ -55,10 +67,15 @@ func (f IntFlag) Default() interface{} {
 	return f.Value
 }
 
+func (f IntFlag) IsOptional() bool {
+	return f.Optional
+}
+
 type BoolFlag struct {
-	Name   string
-	Usage  string
-	EnvVar string
+	Name     string
+	Usage    string
+	EnvVar   string
+	Optional bool
 }
 
 // TODO: Could this be done more succinctly using embedding?
@@ -68,4 +85,8 @@ func (f BoolFlag) String() string {
 
 func (f BoolFlag) Default() interface{} {
 	return false
+}
+
+func (f BoolFlag) IsOptional() bool {
+	return f.Optional
 }
